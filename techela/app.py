b2a226140eb14802d1f7d71c4e4f68d12c78de7e
John Kitchin <jkitchin@andrew.cmu.edu>
2018-08-22 09:34:52 -0400

    simplify app so it doesn't lanuch two tabs

--- techela/app.py
+++ techela/app.py
@@ -6,35 +6,35 @@ import webbrowser
 import socket
 import errno
 
-
+RUNNING = False
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 port = 5543
 
-# Get a free port.
-while True:
-    try:
-        s.bind(("127.0.0.1", port))
-        break
-    except socket.error as e:
-        if e.errno == errno.EADDRINUSE:
-            print("Port is already in use")
-            port += 1
-        else:
-            # something else raised the socket.error exception
-            print(e)
+# # Get a free port.
+# while not RUNNING:
+#     try:
+#         s.bind(("127.0.0.1", port))
+#         RUNNING = True
+#         break
+#     except socket.error as e:
+#         if e.errno == errno.EADDRINUSE:
+#             print("Port is already in use")
+#             port += 1
+#         else:
+#             # something else raised the socket.error exception
+#             print(e)
 
-s.close()
+# s.close()
 
 url = "http://127.0.0.1:{0}".format(port)
 
 # I don't recall exactly why this step is needed. It must be you need to be
 # sure the browser is open first.
+
 threading.Timer(1.25, lambda: webbrowser.open(url)).start()
 
-# We start in the assignments directory
-os.chdir(os.path.expanduser(f'{techela.COURSEDIR}/assignments'))
+# We start in the assignments directory. I do not remember why.
+# os.chdir(os.path.expanduser(f'{techela.COURSEDIR}/assignments'))
 
 techela.app.run(port=port, debug=True)
-print(os.path.expanduser(f'{techela.COURSEDIR}/assignments'),
-      os.getcwd())
