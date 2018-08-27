f99fd8daff3ef2d0bfd7de6af219e6b05d11ff2f
Zachary Ulissi <zulissi@gmail.com>
2018-08-27 11:14:22 -0400

    Update app.py

--- techela/app.py
+++ techela/app.py
@@ -3,7 +3,8 @@ import threading
 import webbrowser
 
 port = 5543
-url = "http://127.0.0.1:{0}".format(port)
+host = '0.0.0.0' # use 0.0.0.0 to listen on all ports, not just for local connections
+url = "http://{0}:{1}".format(host,port)
 
 threading.Timer(1.25, lambda: webbrowser.open(url)).start()
-techela.app.run(port=port, debug=True, use_reloader=False)
+techela.app.run(host=host, port=port, debug=True, use_reloader=False)
