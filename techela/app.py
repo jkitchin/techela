a6e9e64256ef6dfe56b45ce78ff3cd10cb89867a
John Kitchin <jkitchin@andrew.cmu.edu>
2018-09-04 16:27:09 -0400

    change to any port.
    Zack suggested this for compatibility with Docker.

--- techela/app.py
+++ techela/app.py
@@ -3,7 +3,7 @@ import threading
 import webbrowser
 
 port = 5543
-url = "http://127.0.0.1:{0}".format(port)
+url = f"http://0.0.0.0:{port}"
 
 threading.Timer(1.25, lambda: webbrowser.open(url)).start()
 techela.app.run(port=port, debug=True, use_reloader=False)
