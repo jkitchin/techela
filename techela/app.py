f8c196c948a9b6b149fa1003d02bf75504140e6d
John Kitchin <jkitchin@andrew.cmu.edu>
2018-09-07 15:00:27 -0400

    rename port to go back to working.

--- techela/app.py
+++ techela/app.py
@@ -3,7 +3,7 @@ import threading
 import webbrowser
 
 port = 5543
-url = f"http://0.0.0.0:{port}"
+url = f"http://127.0.0.1:{port}"
 
 threading.Timer(1.25, lambda: webbrowser.open(url)).start()
 techela.app.run(port=port, debug=True, use_reloader=False)
